d4664de43eee701128e2094c9e5bec1d74738c8d
Andre Seiji Lima <52793849+askawasaki@users.noreply.github.com>
2020-05-23 18:49:54 -0300

    SemPastaSite
    Pasta Site removida

--- SITE/CONTROLLER/conectar.php
+++
@@ -1,9 +0,0 @@
-<?php 
-define('HOST', '127.0.0.1');
-define('USUARIO', 'root');
-define('SENHA','');
-define('DB', 'TimeLearner');
-
-$conexao = mysqli_connect(HOST,USUARIO,SENHA,DB) or die ('Não foi possivel conectar');
-
-	?>
